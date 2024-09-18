d8e210c0ede07e1b0cf55de812d3f689034d0f18
Grant Cawley <grantlcawley@gmail.com>
2024-09-18 14:50:13 -0400

    feat: application backend setup

---
+++ server/src/server.tsx
@@ -0,0 +1,34 @@
+import express, { Express, Request, Response } from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const uri: string =
+    process.env.MONGODB_URI || 'mongodb://localhost:27017/your-app';
+
+(async () => {
+    try {
+        await mongoose.connect(uri);
+        console.log('Connected to the database');
+    } catch(error) {
+        console.error(error);
+    }
+})();
+
+app.get('/health', (_req: Request, res: Response) => {
+    res.status(200).send('Server is running');
+});
+
+const PORT: string | number = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT: ${PORT}`);
+});
