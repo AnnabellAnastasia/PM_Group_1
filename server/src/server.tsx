9a85f69972abf2e2b10bc87100eecd0cc2411753
Grant Cawley <grantlcawley@gmail.com>
2024-09-19 11:59:30 -0400

    feat: MVC back end with get and post endpoints

--- server/src/server.tsx
+++ server/src/server.tsx
@@ -2,6 +2,7 @@ import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/userRoutes';
 
 dotenv.config();
 
@@ -12,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const uri: string =
-    process.env.MONGODB_URI || 'mongodb://localhost:27017/your-app';
+    process.env.MONGODB_URI || 'mongodb://localhost:27017/ninerNetworking';
 
 (async () => {
     try {
@@ -22,6 +23,8 @@ const uri: string =
         console.error(error);
     }
 })();
+
+app.use('/users', userRoutes);
 
 app.get('/health', (_req: Request, res: Response) => {
     res.status(200).send('Server is running');
