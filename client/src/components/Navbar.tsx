4cfcef461dc1f93fe70db7af9fd42ded2da23998
gabriel abbate <abbategabriel@gmail.com>
2024-10-03 15:33:49 -0400

    initial commit

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <nav>
+            <h2 id="logo">Niner Network</h2>
+            <a href="/" className="navLink">Link</a>
+        </nav>
+    );
+}
+
+export default Navbar; 
+
