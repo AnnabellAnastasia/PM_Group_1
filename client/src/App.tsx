f78b3aad6ce1ba266422bfbc0c3e97eaa7e31e3e
Grant Cawley <grantlcawley@gmail.com>
2024-10-04 14:57:57 -0400

    Basic post feed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import CreateUser from './components/CreateUser';
 import NewComponent from './components/NewComponent';
+import PostFeed from './components/PostFeed';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-			<CreateUser />
-			<NewComponent />
+			{/* <CreateUser /> */}
+			{/* <NewComponent /> */}
+			<PostFeed />
     </div>
   );
 }
