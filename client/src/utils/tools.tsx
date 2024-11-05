b089cb00759be75f9472dad36c47f212d62e3617
Grant Cawley <grantlcawley@gmail.com>
2024-11-05 07:12:11 -0500

    feat: capitalize tool

--- client/src/utils/tools.tsx
+++ client/src/utils/tools.tsx
@@ -3,3 +3,7 @@ import { format } from 'date-fns';
 export function processDate(dateObj: any) {
 	return format(new Date(dateObj), "PPP");
 }
+
+export function capitalize(input: string) {
+	return input.replace(/^\w/, (c) => c.toUpperCase());
+}
